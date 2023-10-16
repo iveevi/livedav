e0ed2f854913f46f9def28e8f57863939a89b5f7
Venkataram <vesion4690@gmail.com>
2023-10-15 23:30:09 -0700

    Cleanup update

--- config.h
+++ config.h
@@ -34,22 +34,22 @@
 // These are in order of least-favourable to most-favourable choices, in case
 // multiple are supported and configured.
 enum input_method {
-    INPUT_FIFO,
-    INPUT_PORTAUDIO,
-    INPUT_ALSA,
-    INPUT_PULSE,
-    INPUT_SNDIO,
-    INPUT_SHMEM,
-    INPUT_MAX
+	INPUT_FIFO,
+	INPUT_PORTAUDIO,
+	INPUT_ALSA,
+	INPUT_PULSE,
+	INPUT_SNDIO,
+	INPUT_SHMEM,
+	INPUT_MAX
 };
 
 enum output_method {
-    OUTPUT_NCURSES,
-    OUTPUT_NONCURSES,
-    OUTPUT_RAW,
-    OUTPUT_SDL,
-    OUTPUT_NORITAKE,
-    OUTPUT_NOT_SUPORTED
+	OUTPUT_NCURSES,
+	OUTPUT_NONCURSES,
+	OUTPUT_RAW,
+	OUTPUT_SDL,
+	OUTPUT_NORITAKE,
+	OUTPUT_NOT_SUPORTED
 };
 
 enum mono_option { LEFT, RIGHT, AVERAGE };
@@ -58,26 +58,24 @@ enum data_format { FORMAT_ASCII = 0, FORMAT_BINARY = 1, FORMAT_NTK3000 = 2 };
 enum xaxis_scale { NONE, FREQUENCY, NOTE };
 
 struct config_params {
-    char *color, *bcolor, *raw_target, *audio_source,
-        /**gradient_color_1, *gradient_color_2,*/ **gradient_colors, *data_format;
-    char bar_delim, frame_delim;
-    double monstercat, integral, gravity, ignore, sens, noise_reduction;
-    unsigned int lower_cut_off, upper_cut_off;
-    double *userEQ;
-    enum input_method input;
-    enum output_method output;
-    enum xaxis_scale xaxis;
-    enum mono_option mono_opt;
-    int userEQ_keys, userEQ_enabled, col, bgcol, autobars, stereo, raw_format, ascii_range,
-        bit_format, gradient, gradient_count, fixedbars, framerate, bar_width, bar_spacing,
-        bar_height, autosens, overshoot, waves, fifoSample, fifoSampleBits, sleep_timer, sdl_width,
-        sdl_height, sdl_x, sdl_y, draw_and_quit, zero_test, non_zero_test, reverse, sync_updates;
+	char *color, *bcolor, *raw_target, *audio_source, **gradient_colors, *data_format;
+	char bar_delim, frame_delim;
+	double monstercat, integral, gravity, ignore, sens, noise_reduction;
+	unsigned int lower_cut_off, upper_cut_off;
+	double *userEQ;
+	enum input_method input;
+	enum output_method output;
+	enum xaxis_scale xaxis;
+	enum mono_option mono_opt;
+	int userEQ_keys, userEQ_enabled, col, bgcol, autobars, stereo, raw_format, ascii_range,
+	    bit_format, gradient, gradient_count, fixedbars, framerate, bar_width, bar_spacing,
+	    bar_height, autosens, overshoot, waves, sleep_timer, draw_and_quit, zero_test, non_zero_test, reverse, sync_updates;
 };
 
 struct error_s {
-    char message[MAX_ERROR_LEN];
-    int length;
+	char message[MAX_ERROR_LEN];
+	int length;
 };
 
 bool load_config(char configPath[PATH_MAX], struct config_params *p, bool colorsOnly,
-                 struct error_s *error);
+		struct error_s *error);
--- output/raw.c
+++ output/raw.c
@@ -4,26 +4,26 @@
 #include <unistd.h>
 
 int print_raw_out(int bars_count, int fd, int is_binary, int bit_format, int ascii_range,
-	char bar_delim, char frame_delim, int const f[]) {
-    int16_t buf_16;
-    int8_t buf_8;
+		char bar_delim, char frame_delim, int const f[]) {
+	int16_t buf_16;
+	int8_t buf_8;
 
-    for (int i = 0; i < bars_count; i++) {
-	int f_ranged = f[i];
-	//if (f_ranged > ascii_range)
-	//    f_ranged = ascii_range;
+	for (int i = 0; i < bars_count; i++) {
+		int f_ranged = f[i];
+		//if (f_ranged > ascii_range)
+		//    f_ranged = ascii_range;
 
-	// finding size of number-string in byte
-	int bar_height_size = 2; // a number + \0
-	if (f_ranged != 0)
-	    bar_height_size += floor(log10(f_ranged));
+		// finding size of number-string in byte
+		int bar_height_size = 2; // a number + \0
+		if (f_ranged != 0)
+			bar_height_size += floor(log10(f_ranged));
 
-	char bar_height[bar_height_size];
-	snprintf(bar_height, bar_height_size, "%d", f_ranged);
+		char bar_height[bar_height_size];
+		snprintf(bar_height, bar_height_size, "%d", f_ranged);
 
-	write(fd, bar_height, bar_height_size - 1);
-	write(fd, &bar_delim, sizeof(bar_delim));
-    }
-    write(fd, &frame_delim, sizeof(frame_delim));
-    return 0;
+		write(fd, bar_height, bar_height_size - 1);
+		write(fd, &bar_delim, sizeof(bar_delim));
+	}
+	write(fd, &frame_delim, sizeof(frame_delim));
+	return 0;
 }
